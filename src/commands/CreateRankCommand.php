8445b9dbdfcd1f7b419e248c586f94f342e23c5c
KanadeBlue <124839201+KanadeBlue@users.noreply.github.com>
2024-08-06 00:29:15 +0100

    ee

--- src/commands/CreateRankCommand.php
+++ src/commands/CreateRankCommand.php
@@ -30,10 +30,10 @@ class CreateRankCommand extends Command {
         array_unshift($rankNames, "None");
 
         $chatFormats = [
-            "&0 - Black", "&1 - Dark Blue", "&2 - Dark Green", "&3 - Dark Aqua",
-            "&4 - Dark Red", "&5 - Dark Purple", "&6 - Gold", "&7 - Gray",
-            "&8 - Dark Gray", "&9 - Blue", "&a - Green", "&b - Aqua",
-            "&c - Red", "&d - Light Purple", "&e - Yellow", "&f - White",
+            "§0 - Black", "§1 - Dark Blue", "§2 - Dark Green", "§3 - Dark Aqua",
+            "§4 - Dark Red", "§5 - Dark Purple", "§6 - Gold", "§7 - Gray",
+            "§8 - Dark Gray", "§9 - Blue", "§a - Green", "§b - Aqua",
+            "§c - Red", "§d - Light Purple", "§e - Yellow", "§f - White",
             "None"
         ];
 
